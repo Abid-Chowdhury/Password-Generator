d0183f0cd4ebf6ce90a659365994217ca40011bb
Abid Chowdhury <58383484+Abid1732@users.noreply.github.com>
2022-05-25 02:48:05 -0400

    Renamed ui components

--- GUI/ui.py
+++ GUI/ui.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'designzbzOKw.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
 from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
     QRect, QSize, QUrl, Qt)
 from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
@@ -23,26 +33,26 @@ class Ui_MainWindow(object):
 "}")
         self.frame.setFrameShape(QFrame.StyledPanel)
         self.frame.setFrameShadow(QFrame.Raised)
-        self.label = QLabel(self.frame)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(0, 0, 500, 75))
+        self.label_Title = QLabel(self.frame)
+        self.label_Title.setObjectName(u"label_Title")
+        self.label_Title.setGeometry(QRect(0, 0, 500, 75))
         font = QFont()
         font.setFamily(u"Arial")
         font.setPointSize(24)
         font.setBold(True)
         font.setWeight(75)
-        self.label.setFont(font)
-        self.label.setStyleSheet(u"QLabel{\n"
+        self.label_Title.setFont(font)
+        self.label_Title.setStyleSheet(u"QLabel{\n"
 "	color: rgb(255, 255, 255);\n"
 "} ")
-        self.label.setFrameShape(QFrame.NoFrame)
-        self.label.setFrameShadow(QFrame.Plain)
-        self.label.setLineWidth(5)
-        self.label.setAlignment(Qt.AlignCenter)
-        self.horizontalSlider = QSlider(self.frame)
-        self.horizontalSlider.setObjectName(u"horizontalSlider")
-        self.horizontalSlider.setGeometry(QRect(50, 200, 150, 30))
-        self.horizontalSlider.setStyleSheet(u"QSlider::groove {\n"
+        self.label_Title.setFrameShape(QFrame.NoFrame)
+        self.label_Title.setFrameShadow(QFrame.Plain)
+        self.label_Title.setLineWidth(5)
+        self.label_Title.setAlignment(Qt.AlignCenter)
+        self.horizontalSlider_Length = QSlider(self.frame)
+        self.horizontalSlider_Length.setObjectName(u"horizontalSlider_Length")
+        self.horizontalSlider_Length.setGeometry(QRect(50, 200, 150, 30))
+        self.horizontalSlider_Length.setStyleSheet(u"QSlider::groove {\n"
 "    border-radius: 5px;\n"
 "    height: 10px;\n"
 "	margin: 0px;\n"
@@ -57,17 +67,17 @@ class Ui_MainWindow(object):
 "    margin: 0px;\n"
 "	border-radius: 5px;\n"
 "}")
-        self.horizontalSlider.setOrientation(Qt.Horizontal)
-        self.checkBox_4 = QCheckBox(self.frame)
-        self.checkBox_4.setObjectName(u"checkBox_4")
-        self.checkBox_4.setGeometry(QRect(300, 135, 150, 30))
+        self.horizontalSlider_Length.setOrientation(Qt.Horizontal)
+        self.checkBox_Uppercase = QCheckBox(self.frame)
+        self.checkBox_Uppercase.setObjectName(u"checkBox_Uppercase")
+        self.checkBox_Uppercase.setGeometry(QRect(300, 135, 150, 30))
         font1 = QFont()
         font1.setFamily(u"Arial")
         font1.setPointSize(12)
         font1.setBold(False)
         font1.setWeight(50)
-        self.checkBox_4.setFont(font1)
-        self.checkBox_4.setStyleSheet(u"QCheckBox{\n"
+        self.checkBox_Uppercase.setFont(font1)
+        self.checkBox_Uppercase.setStyleSheet(u"QCheckBox{\n"
 "	color: white;\n"
 "}\n"
 "\n"
@@ -83,14 +93,14 @@ class Ui_MainWindow(object):
 "    background: 3px solid rgb(189, 147, 249);\n"
 "	border: 3px solid rgb(52, 59, 72);	\n"
 "}")
-        self.checkBox_4.setChecked(True)
-        self.checkBox_4.setAutoExclusive(False)
-        self.checkBox_4.setTristate(False)
-        self.checkBox_5 = QCheckBox(self.frame)
-        self.checkBox_5.setObjectName(u"checkBox_5")
-        self.checkBox_5.setGeometry(QRect(300, 170, 150, 30))
-        self.checkBox_5.setFont(font1)
-        self.checkBox_5.setStyleSheet(u"QCheckBox{\n"
+        self.checkBox_Uppercase.setChecked(True)
+        self.checkBox_Uppercase.setAutoExclusive(False)
+        self.checkBox_Uppercase.setTristate(False)
+        self.checkBox_Lowercase = QCheckBox(self.frame)
+        self.checkBox_Lowercase.setObjectName(u"checkBox_Lowercase")
+        self.checkBox_Lowercase.setGeometry(QRect(300, 170, 150, 30))
+        self.checkBox_Lowercase.setFont(font1)
+        self.checkBox_Lowercase.setStyleSheet(u"QCheckBox{\n"
 "	color: white;\n"
 "}\n"
 "\n"
@@ -106,14 +116,14 @@ class Ui_MainWindow(object):
 "    background: 3px solid rgb(189, 147, 249);\n"
 "	border: 3px solid rgb(52, 59, 72);	\n"
 "}")
-        self.checkBox_5.setChecked(True)
-        self.checkBox_5.setAutoExclusive(False)
-        self.checkBox_5.setTristate(False)
-        self.checkBox_6 = QCheckBox(self.frame)
-        self.checkBox_6.setObjectName(u"checkBox_6")
-        self.checkBox_6.setGeometry(QRect(300, 205, 150, 30))
-        self.checkBox_6.setFont(font1)
-        self.checkBox_6.setStyleSheet(u"QCheckBox{\n"
+        self.checkBox_Lowercase.setChecked(True)
+        self.checkBox_Lowercase.setAutoExclusive(False)
+        self.checkBox_Lowercase.setTristate(False)
+        self.checkBox_Numbers = QCheckBox(self.frame)
+        self.checkBox_Numbers.setObjectName(u"checkBox_Numbers")
+        self.checkBox_Numbers.setGeometry(QRect(300, 205, 150, 30))
+        self.checkBox_Numbers.setFont(font1)
+        self.checkBox_Numbers.setStyleSheet(u"QCheckBox{\n"
 "	color: white;\n"
 "}\n"
 "\n"
@@ -129,14 +139,14 @@ class Ui_MainWindow(object):
 "    background: 3px solid rgb(189, 147, 249);\n"
 "	border: 3px solid rgb(52, 59, 72);	\n"
 "}")
-        self.checkBox_6.setChecked(True)
-        self.checkBox_6.setAutoExclusive(False)
-        self.checkBox_6.setTristate(False)
-        self.checkBox_7 = QCheckBox(self.frame)
-        self.checkBox_7.setObjectName(u"checkBox_7")
-        self.checkBox_7.setGeometry(QRect(300, 240, 150, 30))
-        self.checkBox_7.setFont(font1)
-        self.checkBox_7.setStyleSheet(u"QCheckBox{\n"
+        self.checkBox_Numbers.setChecked(True)
+        self.checkBox_Numbers.setAutoExclusive(False)
+        self.checkBox_Numbers.setTristate(False)
+        self.checkBox_Symbols = QCheckBox(self.frame)
+        self.checkBox_Symbols.setObjectName(u"checkBox_Symbols")
+        self.checkBox_Symbols.setGeometry(QRect(300, 240, 150, 30))
+        self.checkBox_Symbols.setFont(font1)
+        self.checkBox_Symbols.setStyleSheet(u"QCheckBox{\n"
 "	color: white;\n"
 "}\n"
 "\n"
@@ -152,19 +162,19 @@ class Ui_MainWindow(object):
 "    background: 3px solid rgb(189, 147, 249);\n"
 "	border: 3px solid rgb(52, 59, 72);	\n"
 "}")
-        self.checkBox_7.setChecked(True)
-        self.checkBox_7.setAutoExclusive(False)
-        self.checkBox_7.setTristate(False)
-        self.pushButton = QPushButton(self.frame)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(185, 300, 130, 50))
+        self.checkBox_Symbols.setChecked(True)
+        self.checkBox_Symbols.setAutoExclusive(False)
+        self.checkBox_Symbols.setTristate(False)
+        self.pushButton_Generate = QPushButton(self.frame)
+        self.pushButton_Generate.setObjectName(u"pushButton_Generate")
+        self.pushButton_Generate.setGeometry(QRect(185, 300, 130, 50))
         font2 = QFont()
         font2.setFamily(u"Arial")
         font2.setPointSize(12)
         font2.setBold(True)
         font2.setWeight(75)
-        self.pushButton.setFont(font2)
-        self.pushButton.setStyleSheet(u"QPushButton {\n"
+        self.pushButton_Generate.setFont(font2)
+        self.pushButton_Generate.setStyleSheet(u"QPushButton {\n"
 "	color: white;\n"
 "	border: 2px solid rgb(52, 59, 72);\n"
 "	border-radius: 25px;	\n"
@@ -187,32 +197,32 @@ class Ui_MainWindow(object):
 "}")
         self.line.setFrameShape(QFrame.VLine)
         self.line.setFrameShadow(QFrame.Sunken)
-        self.label_2 = QLabel(self.frame)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(50, 135, 150, 30))
+        self.label_Length = QLabel(self.frame)
+        self.label_Length.setObjectName(u"label_Length")
+        self.label_Length.setGeometry(QRect(50, 135, 150, 30))
         font3 = QFont()
         font3.setFamily(u"Arial")
         font3.setPointSize(12)
-        self.label_2.setFont(font3)
-        self.label_2.setStyleSheet(u"QLabel {\n"
+        self.label_Length.setFont(font3)
+        self.label_Length.setStyleSheet(u"QLabel {\n"
 "	color: white;\n"
 "}")
-        self.label_2.setAlignment(Qt.AlignCenter)
-        self.label_3 = QLabel(self.frame)
-        self.label_3.setObjectName(u"label_3")
-        self.label_3.setGeometry(QRect(50, 170, 150, 30))
-        self.label_3.setFont(font2)
-        self.label_3.setStyleSheet(u"QLabel {\n"
+        self.label_Length.setAlignment(Qt.AlignCenter)
+        self.label_Length_Number = QLabel(self.frame)
+        self.label_Length_Number.setObjectName(u"label_Length_Number")
+        self.label_Length_Number.setGeometry(QRect(50, 170, 150, 30))
+        self.label_Length_Number.setFont(font2)
+        self.label_Length_Number.setStyleSheet(u"QLabel {\n"
 "	color: white;\n"
 "}")
-        self.label_3.setAlignment(Qt.AlignCenter)
-        self.lineEdit = QLineEdit(self.frame)
-        self.lineEdit.setObjectName(u"lineEdit")
-        self.lineEdit.setGeometry(QRect(100, 75, 300, 30))
+        self.label_Length_Number.setAlignment(Qt.AlignCenter)
+        self.lineEdit_Password = QLineEdit(self.frame)
+        self.lineEdit_Password.setObjectName(u"lineEdit_Password")
+        self.lineEdit_Password.setGeometry(QRect(100, 75, 300, 30))
         font4 = QFont()
         font4.setPointSize(12)
-        self.lineEdit.setFont(font4)
-        self.lineEdit.setStyleSheet(u"QLineEdit {\n"
+        self.lineEdit_Password.setFont(font4)
+        self.lineEdit_Password.setStyleSheet(u"QLineEdit {\n"
 "	color: white;\n"
 "	background-color: rgb(33, 37, 43);\n"
 "	border-radius: 5px;\n"
@@ -227,27 +237,27 @@ class Ui_MainWindow(object):
 "QLineEdit:focus {\n"
 "	border: 2px solid rgb(91, 101, 124);\n"
 "}")
-        self.lineEdit.setAlignment(Qt.AlignCenter)
-        self.lineEdit.setDragEnabled(False)
-        self.lineEdit.setCursorMoveStyle(Qt.VisualMoveStyle)
-        self.pushButton_2 = QPushButton(self.frame)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(470, 15, 14, 14))
-        self.pushButton_2.setStyleSheet(u"QPushButton {\n"
+        self.lineEdit_Password.setAlignment(Qt.AlignCenter)
+        self.lineEdit_Password.setDragEnabled(False)
+        self.lineEdit_Password.setCursorMoveStyle(Qt.VisualMoveStyle)
+        self.pushButton_Close = QPushButton(self.frame)
+        self.pushButton_Close.setObjectName(u"pushButton_Close")
+        self.pushButton_Close.setGeometry(QRect(470, 15, 14, 14))
+        self.pushButton_Close.setStyleSheet(u"QPushButton {\n"
 "	border-radius: 7px;\n"
 "	background-color: rgb(255,0,0);\n"
 "}")
-        self.pushButton_3 = QPushButton(self.frame)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(445, 15, 14, 14))
-        self.pushButton_3.setStyleSheet(u"QPushButton {\n"
+        self.pushButton_Maximize = QPushButton(self.frame)
+        self.pushButton_Maximize.setObjectName(u"pushButton_Maximize")
+        self.pushButton_Maximize.setGeometry(QRect(445, 15, 14, 14))
+        self.pushButton_Maximize.setStyleSheet(u"QPushButton {\n"
 "	border-radius: 7px;\n"
 "	background-color: rgb(0,255,0);\n"
 "}")
-        self.pushButton_4 = QPushButton(self.frame)
-        self.pushButton_4.setObjectName(u"pushButton_4")
-        self.pushButton_4.setGeometry(QRect(420, 15, 14, 14))
-        self.pushButton_4.setStyleSheet(u"QPushButton {\n"
+        self.pushButton_Minimize = QPushButton(self.frame)
+        self.pushButton_Minimize.setObjectName(u"pushButton_Minimize")
+        self.pushButton_Minimize.setGeometry(QRect(420, 15, 14, 14))
+        self.pushButton_Minimize.setStyleSheet(u"QPushButton {\n"
 "	border-radius: 7px;\n"
 "	background-color: rgb(255,255,0);\n"
 "}")
@@ -260,18 +270,18 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Password Generator", None))
-        self.checkBox_4.setText(QCoreApplication.translate("MainWindow", u"Uppercase", None))
-        self.checkBox_5.setText(QCoreApplication.translate("MainWindow", u"Lowercase", None))
-        self.checkBox_6.setText(QCoreApplication.translate("MainWindow", u"Numbers", None))
-        self.checkBox_7.setText(QCoreApplication.translate("MainWindow", u"Symbols", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Generate", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Length", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"8", None))
-        self.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Password", None))
-        self.pushButton_2.setText("")
-        self.pushButton_3.setText("")
-        self.pushButton_4.setText("")
+        self.label_Title.setText(QCoreApplication.translate("MainWindow", u"Password Generator", None))
+        self.checkBox_Uppercase.setText(QCoreApplication.translate("MainWindow", u"Uppercase", None))
+        self.checkBox_Lowercase.setText(QCoreApplication.translate("MainWindow", u"Lowercase", None))
+        self.checkBox_Numbers.setText(QCoreApplication.translate("MainWindow", u"Numbers", None))
+        self.checkBox_Symbols.setText(QCoreApplication.translate("MainWindow", u"Symbols", None))
+        self.pushButton_Generate.setText(QCoreApplication.translate("MainWindow", u"Generate", None))
+        self.label_Length.setText(QCoreApplication.translate("MainWindow", u"Length", None))
+        self.label_Length_Number.setText(QCoreApplication.translate("MainWindow", u"8", None))
+        self.lineEdit_Password.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Password", None))
+        self.pushButton_Close.setText("")
+        self.pushButton_Maximize.setText("")
+        self.pushButton_Minimize.setText("")
     # retranslateUi
 
     def return_Status():
